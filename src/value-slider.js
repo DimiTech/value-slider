18db59be6b6d9cab1a4352418cc1913eece15750
DusanDimitric <dusan_dimitric@yahoo.com>
2016-06-14 14:00:29 +0200

    Use <input> parent instead of a <div>

--- src/value-slider.js
+++ src/value-slider.js
@@ -6,13 +6,13 @@
 	*	Widget constructor
 	*
 	*	@class ValueSlider
-	*	@classdesc Creates a new value slider widget inside the given <div> element
+	*	@classdesc Creates a new value slider widget around the given <input> element
 	*	
 	*	@param {Object} options - Options for initializing the component
 	*/
 	var ValueSlider = function(options) {
 
-		setParentDiv(this, options.div);
+		setParentDiv(this, options.element);
 		
 		createWidget(this);
 
@@ -20,7 +20,14 @@
 							   options.maxValue || 100, 
 							   options.step     || 1   );
 
-		this.setValue(options.value || 0); // Short-circuit evaluate the value value
+		/** 
+		*	Short-circuit evaluate the widget value
+		*   If no value property is specified on the options object take the value from the parent <input>'s value attribute. 
+		*   If <input> doesn't have a value then set the value to 0. 
+		*/
+		this.setValue(options.value ||
+					  parseInt(this.parentDiv.getElementsByTagName('input')[0].defaultValue) ||
+					  0);
 
 		setEventListeners(this);
 
@@ -30,14 +37,33 @@
 
 	/* ------------------------------------- Private functions ------------------------------------- */
 
-	// Handles declaring the user given <div> element as the parentElement property 
-	function setParentDiv(self, parentDiv) {
-		if (parentDiv[0] !== undefined && parentDiv[0].tagName === 'DIV') // Handle the jQuery selector
-			self.parentElement = parentDiv[0];
-		else if (parentDiv.tagName === 'DIV') // Handle document.getElementById()
-			self.parentElement = parentDiv;
-		else // The user hasn't supplied a proper parent <div> element
-			throw new Error('Please supply a <div> element in which you wish to load the "ValueSlider" widget into.');
+	// Insert desc
+	function setParentDiv(self, parentInput) {
+
+		var inputElem; // Our widget's input element (which will store it's value, useful for POST requests)
+
+		if (parentInput[0] !== undefined && parentInput[0].tagName === 'INPUT') // Handle the jQuery selector
+			inputElem = parentInput[0];
+		else if (parentInput.tagName === 'INPUT') // Handle document.getElementById()
+			inputElem = parentInput;
+		else // The user hasn't supplied a proper parent <input> element
+			throw new Error('Please supply an <input> element in which you wish to load the "ValueSlider" widget into.');
+
+		// Wrap the <input> element in a <div>
+		var parentDiv = document.createElement('div');
+		parentDiv.className = 'value-slider';
+
+		// Insert our wrapper <div> element before the <input>
+		inputElem.parentNode.insertBefore(parentDiv, inputElem);
+
+		// Move the <input> into the wrapper <div>
+		parentDiv.appendChild(inputElem);
+
+		// Hide the <input> element, we don't really want to see it
+		inputElem.style.display = 'none';
+
+		// Save a reference to the wrapping <div> as a property
+		self.parentDiv = parentDiv;
 	}
 
 	// Creates HTML elements needed for the widget
@@ -45,7 +71,7 @@
 		// Create a container <div>
 		self.widget = document.createElement('div');
 		self.widget.className = 'value-slider-body'; 
-		self.parentElement.appendChild(self.widget);
+		self.parentDiv.appendChild(self.widget);
 
 		// Hide the widget until it's fully loaded
 		self.hide();
@@ -81,14 +107,9 @@
 	function renderValue(self) {
 		var valueSliderDiv = self.widget.children[0];
 
-		if (self.step !== 0)
-			calculateSteps(self);
-
 		var value = ~~((self.rightValue - self.minValue) * 100 / (self.maxValue - self.minValue));
 
 		valueSliderDiv.style.width = value + '%';
-
-		console.log(self.rightValue);
 	}
 
 	// Take step into account
@@ -100,6 +121,12 @@
 			self.rightValue = self.rightValue - stepRemainder + self.step;
 		else
 			self.rightValue = self.rightValue - stepRemainder;
+	}
+
+	// Update the <input> element's value
+	function updateInputValue(self) {
+		self.parentDiv.querySelector('input').value 	   = self.rightValue;	
+		self.parentDiv.querySelector('input').defaultValue = self.rightValue;
 	}
 
 	function clipValue(value, min, max, toInt) {
@@ -147,7 +174,8 @@
 					var mousePosition = getPositionOnSlider(self, event.clientX);
 
 					mousePosition = clipValue(mousePosition, 0, 1, false);
-				
+					
+					// Update the value
 					self.setRightPercent(mousePosition);
 				}
 			});
@@ -202,6 +230,12 @@
 			// Set the value
 			this.rightValue = clipValue(value, 0, this.maxValue, true);
 
+
+			if (this.step !== 0)
+				calculateSteps(this);
+
+			updateInputValue(this);
+
 			// Render the widget		
 			renderValue(this);
 		} else 
@@ -216,6 +250,12 @@
 		if (typeof value === 'number') {
 
 			this.rightValue = clipValue(value, this.minValue, this.maxValue, true); // Set the value
+
+			if (this.step !== 0)
+				calculateSteps(this);
+
+			updateInputValue(this);
+
 			renderValue(this); // Render the widget
 		} else 
 			throw new Error((funcName || 'setRightValue()') + ' expects a number parameter! (a value from ' + this.minValue + ' to ' + this.maxValue + ')');
